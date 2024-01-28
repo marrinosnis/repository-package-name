689be02422989f8a07b8d93950c8908e96e11a97
marinos <mr.nisiotis@gmail.com>
2024-01-28 19:40:03 +0200

    use another type of query

--- findRepoPackageName.sh
+++ findRepoPackageName.sh
@@ -28,8 +28,18 @@ elif [ -n "$owner" ] && [ -z "$inputToken" ]; then
   echo -e "You should provide also the token as the second argument\n"
 fi
 
-query='{"query": "{ repository(owner: \"'"${owner}"'\", name: \"'"${repoName}"'\"){ packages(first: 1) { nodes { name } } } }" }'
-echo $query > query.json
+query='
+query repoPackageName {
+  repository(owner: \"'"${owner}"'\", name: \"'"${repoName}"'\") {
+    packages(first: 1) {
+      nodes {
+        name
+      }
+    }
+  }
+}
+'
+graphQL_query="$(echo $query)"
 
 graphQL_endpoint="https://api.github.com/graphql"
 
@@ -38,8 +48,22 @@ token="${inputToken}"
 graphQL_response=$(curl -s -X POST \
   -H "Authorization: bearer $token" \
   -H "Content-Type: application/json" \
-  -d "@query.json" \
+  -d "{\"query\":\"$graphQL_query\"}" \
   $graphQL_endpoint)
+
+
+# query='{"query": "{ repository(owner: \"'"${owner}"'\", name: \"'"${repoName}"'\"){ packages(first: 1) { nodes { name } } } }" }'
+# echo $query > query.json
+
+# graphQL_endpoint="https://api.github.com/graphql"
+
+# token="${inputToken}"
+
+# graphQL_response=$(curl -s -X POST \
+#   -H "Authorization: bearer $token" \
+#   -H "Content-Type: application/json" \
+#   -d "@query.json" \
+#   $graphQL_endpoint)
 
 name=$(echo "$graphQL_response" | grep -o '"name":"[^"]*' | awk -F'"' '{print $4}')
 echo "$name"
